b90231a80359582c997554d1b4a31718d32cfd40
RaquibTaj <84203410+RaquibTaj@users.noreply.github.com>
2024-06-28 19:03:14 -0400

    Added evaluator class

--- game.py
+++ game.py
@@ -2,11 +2,19 @@ import torch
 from state import State 
 from train import Net
 
+class Evaluator(object):
+    
+    def __init__(self):
+        vals = torch.load("saved_model/weights.pth", map_location=lambda storage, loc: storage)
+        self.model = Net()
+        self.model.load_state_dict(vals)
+
+    def __call__(self, s):
+        board = s.serialize()[None]
+        output = self.model(torch.tensor(board).float())
+        return float(output.data[0][0]) 
+
 if __name__ == "__main__":
-    vals = torch.load("saved_model/weights.pth", map_location=lambda storage, loc: storage)
-    model = Net()
-    model.load_state_dict(vals)
+    e = Evaluator()
     s = State()
-    board = s.serialize()[None]
-    output =model(torch.tensor(board).float())
-    print(output)
+    print(e(s))
